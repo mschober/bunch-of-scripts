250160822e64b4396fed884888c18c5936391e7b
Mike Schober <mschober@cj.com>
2012-05-22 22:57:16 -0700

    missed tracking these deletes

--- insert.js
+++
@@ -1,3 +0,0 @@
-db.getCollection("perf_by_program")
-db.perf_by_program.insert({"cjFee": 0.000, "clicks": 2, "cpm": 0.000, "cr": 0.0, "ctr": 2.0, "epc": 0.000, "imps": 1, "leads": 0, "period": new Date("01/27/2012 00:00:00 UTC-0800"), "publisherCommision": 0.000, "sales": 0, "saleAmount": 0.000, "totalCommision": 0.000})
-
