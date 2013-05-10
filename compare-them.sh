829a37b7c22f25f491f4dae06b433fc9ac2ac7db
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-05-10 16:26:38 -0700

    git status files

--- compare-them.sh
+++ compare-them.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-comm <(sort $1) <(sort $2) | tee result.compared
+OUTFILE=result.comm
+FUNC=sort
+
+usage()
+{
+cat << EOF
+usage: $0 options file1 file2
+
+This script compares to files wrapping the linux 'comm' function.
+
+OPTIONS:
+   -h      Show this message.
+   -o      Specify a new output file. Defaults to $OUTFILE.
+   -f      Specify a different function, note sort is expected. Defaults to $FUNC.
+
+EOF
+}
+
+while getopts "ho:f:" flag; do
+case "$flag" in
+    h) usage && exit1;;
+    o) OUTFILE=$OPTARG;;
+    f) FUNC=$OPTARG;;
+esac
+done
+
+
+[[ $# -lt 2 ]] && usage && exit 1
+FILE1=${@:$OPTIND:1}
+FILE2=${@:$OPTIND+1:1}
+
+comm <($FUNC $FILE1) <($FUNC $FILE2) | tee $FILE
